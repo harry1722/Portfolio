c4f6981ed39f86eb42497eeb8329e6fe9bef5754
harry1722 <raldiraldi42@gmail.com>
2025-05-31 18:23:30 +0200

    Initial commit: added portfolio routes, login page, flash messages

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.secret_key = 'tanismevjennemendjenjesecretkeyndajposhkruajkete'  # vendos ndonjë string të fortë këtu
+
+
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import Flask, render_template,request,flash,redirect, session,url_for
 from app import app
-
+from app.forms import LoginForm
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -13,13 +13,20 @@ def about():
 def projects():
   return  render_template('projects.html')
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET','POST'])
 def contact():
+   if request.method == 'POST':
+      name = request.form.get('name')
+      profession = request.form.get('profession')
+      message = request.form.get('message')
+
+      flash('I got your message.Thank you!', 'success')
+      return redirect(url_for('contact'))
+   
    return render_template('contact.html')
 
-@app.route('/login')
-def login():
-  return  render_template('login.html')
+
+
     
 @app.route('/admin/dashboard')
 def admin_dashboard():
@@ -34,5 +41,22 @@ def admin_messages():
   return  render_template('admin_messages.html')
 
 @app.errorhandler(404)
-def page_not_found():
+def page_not_found(error):
     return render_template('404.html'), 404
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  form = LoginForm()
+  if form.validate_on_submit():
+     username = form.username.data
+     password = form.password.data
+
+     #for database
+     if username == 'admin' and password == 'password123':
+        session['user'] = username
+        flash('You are the right admin!','success')
+        return redirect(url_for('admin_dashboard'))
+     else:
+        flash('Wait!You are not the right admin','danger')
+
+  return  render_template("login.html", form=form)
