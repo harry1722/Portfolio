f35dde3dd0020facf56f00997edab27448cc725d
harry1722 <raldiraldi42@gmail.com>
2025-06-06 16:58:48 +0200

    Updated

--- config.py
+++ config.py
@@ -3,9 +3,13 @@ from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
-
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-if-not-set')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    
+    db_url = os.getenv("DATABASE_URL")
+    if db_url and db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
+    
+    SQLALCHEMY_DATABASE_URI = db_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(basedir, os.getenv('UPLOAD_FOLDER', 'static/uploads'))
