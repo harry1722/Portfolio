8047493b00ab057c8443da021221742524f6e99b
harry1722 <raldiraldi42@gmail.com>
2025-06-07 16:20:28 +0200

    updated again

--- app/forms.py
+++ app/forms.py
@@ -1,27 +1,15 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Length
-from flask_wtf.file import FileField, FileAllowed, FileRequired
+from app import db
+from datetime import datetime
 
-class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    profession = db.Column(db.String(100), nullable=False)
+    message = db.Column(db.Text, nullable=False)
+    Time = db.Column(db.DateTime, default=datetime.utcnow)
 
-
-class ContactForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), Length(max=100)])
-    profession  = StringField('Profession', validators=[DataRequired(), Length(max=100)])
-    message = TextAreaField('Message', validators=[DataRequired()])
-    submit = SubmitField('Send')
-
-class ProjectForm(FlaskForm):
-    title = StringField('Project Title', validators=[DataRequired(), Length(max=120)])
-    description = TextAreaField('Description', validators=[DataRequired(), Length(max=300)])
-    file = FileField('Project File', validators=[
-        FileAllowed(['zip','pdf','py','txt','html','css','js'],'Only zip,pdf,py,txt,html,css are allowed!')
-    ])
-    folder = FileField('Folder Upload', render_kw={'multiple': True}, validators=[
-        FileAllowed(['zip','pdf','py','txt','html','css','js'],'Only zip,pdf,py,txt,html,css are allowed!')
-    ])
-    submit = SubmitField('Add Project')
+class Project(db.Model):
+     id = db.Column(db.Integer, primary_key=True)
+     title = db.Column(db.String(120), nullable=False)
+     description = db.Column(db.String(300), nullable=False)
+     file_name = db.Column(db.String(100),nullable=False)
