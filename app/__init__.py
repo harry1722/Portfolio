f323e751ee210c6981bb6ad93c5fee4b0263060d
harry1722 <raldiraldi42@gmail.com>
2025-06-08 10:43:33 +0200

    changes made

--- app/__init__.py
+++ app/__init__.py
@@ -4,14 +4,19 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
 
+db = SQLAlchemy()
+migrate = Migrate()
 
-app = Flask(__name__, static_folder='../static')
-app.config.from_object(Config)
+def create_app():
+    app = Flask(__name__, static_folder='../static')
+    app.config.from_object(Config)
 
-db = SQLAlchemy(app)
-migrate = Migrate(app,db)
+    db.init_app(app)
+    migrate.init_app(app, db)
 
-upload_folder = app.config['UPLOAD_FOLDER']
-if not os.path.exists(app.config['UPLOAD_FOLDER']):
-    os.makedirs(app.config['UPLOAD_FOLDER'])
-from app import routes, models
+    upload_folder = app.config['UPLOAD_FOLDER']
+    if not os.path.exists(upload_folder):
+        os.makedirs(upload_folder)
+
+    from app import routes, models  # make sure routes/models use `current_app` if needed
+    return app
